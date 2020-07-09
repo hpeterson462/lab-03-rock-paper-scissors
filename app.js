0457492b1f05f4da6a3ade9cdd47a9eac8c49af0
Patrick Wilson <pmwilson333@gmail.com>
2020-07-09 15:15:20 -0700

    refactored and hunted bugs

--- app.js
+++ app.js
@@ -1,46 +1,55 @@
 // import functions and grab DOM elements
-import { getRandomThrow } from './r-p-s.js';
-import { checkResult } from './r-p-s.js';
+import { getRandomThrow, checkResult } from './r-p-s.js';
 
 const playButton = document.getElementById('play-button');
-const gameResults = document.getElementById('results');
-const currentTotal = document.getElementById('current-total');
+const gameResultsSpan = document.getElementById('results');
+const currentTotalSpan = document.getElementById('current-total');
 
 // initialize state
 let winData = 0;
 let drawData = 0;
 let loseData = 0;
-let totalGuesses = 0;
+let totalGuesses = winData + loseData + drawData;
 
 // set event listeners to update state and DOM
 playButton.addEventListener('click', () => {
-    console.log('hello');
 
-    //if total guesses is 3, end game
-    if (totalGuesses === 4) {
-        return;
-    }
-
-    //generate random number
-    getRandomThrow();
+    //generate random throw
+    const randomThrow = getRandomThrow();
 
     //get user input
     const userInput = document.querySelector('input:checked');
-    const userGuess = userInput.value;
-    console.log(userGuess);
+    const userGuess = userInput;
 
-    currentTotal.textContent = totalGuesses++;
+    if (userGuess === null) {
+        alert("Hey, pick a guess!");
+    } else {
+        const userResponse = userGuess.value
+        if (totalGuesses < 3) {
+            totalGuesses++;
+            
+            //compare inputs & add points
+            if (checkResult(randomThrow, userResponse) === 'draw') {
+                drawData++;
+                gameResultsSpan.textContent = 'Draw!';
+            } else if (checkResult(randomThrow, userResponse) === 'win') {
+                winData++;
+                gameResultsSpan.textContent = 'Win!';
+            } else if (checkResult(randomThrow, userResponse) === 'lose') {
+                loseData++;
+                gameResultsSpan.textContent = 'Lose!';
+            }
+        
+            updateNotification();
 
-    //compare inputs & add points
-    if (checkResult() === 'draw') {
-        drawData++;
-        gameResults.textContent = 'Draw';
-    } else if (checkResult() === 'win') {
-        winData++;
-        gameResults.textContent = 'Win!';
-    } else if (checkResult() === 'lose') {
-        loseData++;
-        gameResults.textContent = 'Lose';
+        } else {
+            gameResultsSpan.textContent = "Good Games";
+        }
     }
-    console.log(checkResult());
 });
+
+function updateNotification() {
+    const gameTotalMessage = "Total: " + totalGuesses + "  Wins: " + winData + "  Loses: " + loseData + "  Ties: " + drawData;
+
+    currentTotalSpan.textContent = gameTotalMessage;
+}
